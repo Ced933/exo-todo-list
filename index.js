457a74299da6131055bcc108302c4543ad4b1bbb
Cedric Glou <cedricglou@MacBook-Air-de-Cedric.local>
2024-02-13 12:38:41 +0100

    1 version

--- index.js
+++ index.js
@@ -1,4 +1,36 @@
 "use strict";
 const form = document.querySelector('form');
-const input = document.querySelector("input[type='text']");
-console.log(form, input);
+let input = document.querySelector("input[type='text']");
+const messageBox = document.querySelector(".message-container");
+// variable global qui va servir de pont entre notre e.target.value de notre input et notre fonction   
+let pbridge = "";
+let pArray = [];
+input.addEventListener('input', (e, valueMessage) => {
+    pbridge = e.target.value;
+});
+// je crée une fonction qui va prendre en paramettre notre futur e.target.value car en js les fonctions sont jouées en 1er  
+const addMessage = (valueMessage) => {
+    let p = document.createElement('p');
+    p.innerHTML = valueMessage;
+    messageBox.appendChild(p);
+    localStorage.setItem('value', valueMessage);
+    p.addEventListener('click', (e) => {
+        e.target.remove();
+        localStorage.removeItem('value');
+    });
+};
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    // on appel la fonction addMessage a chaque soumission du formulaire  avec le paramettre pbridge qui de vide est passé a e.target.value
+    if (input.value.length < 2) {
+        alert('votre message doit faire au moins 2 caractères');
+        return false;
+    }
+    else {
+        addMessage(pbridge);
+        // on remet la valeur interne de l'input a 0
+        // input = null
+        // on enleve ce qu'on a tapé précédemment dans l'input 
+        input.value = "";
+    }
+});
--- index.ts
+++ index.ts
@@ -1,3 +1,55 @@
-const form = document.querySelector('form');
-const input = document.querySelector("input[type='text']")
-console.log(form, input);
+const form:any = document.querySelector('form');
+let input:any = document.querySelector("input[type='text']")
+const messageBox:any = document.querySelector(".message-container")
+// variable global qui va servir de pont entre notre e.target.value de notre input et notre fonction   
+let pbridge = "";
+let pArray:any = []
+
+input.addEventListener('input', (e:any,valueMessage:any)=>{
+    pbridge = e.target.value;
+
+})
+
+// je crée une fonction qui va prendre en paramettre notre futur e.target.value car en js les fonctions sont jouées en 1er  
+const addMessage = (valueMessage:any) =>{
+   let p:any = document.createElement('p');
+    p.innerHTML=valueMessage;
+    messageBox.appendChild(p);
+    localStorage.setItem('value', valueMessage)
+
+
+    p.addEventListener('click', (e:any)=>{
+         e.target.remove()
+         localStorage.removeItem('value')
+    })
+ }
+
+
+ 
+
+ 
+
+ 
+         
+     
+ 
+
+form.addEventListener('submit', (e:any)=>{
+    e.preventDefault()
+    // on appel la fonction addMessage a chaque soumission du formulaire  avec le paramettre pbridge qui de vide est passé a e.target.value
+
+    if(input.value.length < 2) {
+        alert('votre message doit faire au moins 2 caractères')
+        return false 
+
+    } else{
+
+        addMessage(pbridge)
+        // on remet la valeur interne de l'input a 0
+        // input = null
+        // on enleve ce qu'on a tapé précédemment dans l'input 
+        input.value="";
+    }
+
+})
+
