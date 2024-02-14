5ab58ded2219c0cd2fe1b89c6a57589f0c12957f
Cedric Glou <cedricglou@MacBook-Air-de-Cedric.local>
2024-02-14 12:58:24 +0100

    amélioration du projet

--- index.js
+++ index.js
@@ -2,35 +2,42 @@
 const form = document.querySelector('form');
 let input = document.querySelector("input[type='text']");
 const messageBox = document.querySelector(".message-container");
-// variable global qui va servir de pont entre notre e.target.value de notre input et notre fonction   
-let pbridge = "";
-let pArray = [];
-input.addEventListener('input', (e, valueMessage) => {
-    pbridge = e.target.value;
-});
+// sauve garder dans le locale storage 
+function storeList() {
+    // todolist est le nom qu'on a donné a notre key ds le localstorage 
+    window.localStorage.todoList = messageBox.innerHTML;
+}
+// récupérer les donner dans le local storage 
+function getStorage() {
+    if (window.localStorage.todoList) {
+        messageBox.innerHTML = window.localStorage.todoList;
+    }
+    else {
+        messageBox.innerHTML = `<p>cliquez sur un todo pour le supprimer</p>`;
+    }
+}
+// au chargement 
+window.addEventListener('load', getStorage);
 // je crée une fonction qui va prendre en paramettre notre futur e.target.value car en js les fonctions sont jouées en 1er  
-const addMessage = (valueMessage) => {
-    let p = document.createElement('p');
-    p.innerHTML = valueMessage;
-    messageBox.appendChild(p);
-    localStorage.setItem('value', valueMessage);
-    p.addEventListener('click', (e) => {
-        e.target.remove();
-        localStorage.removeItem('value');
-    });
-};
+// const addMessage = (valueMessage:any) =>{
+//  }
+messageBox.addEventListener('click', (e) => {
+    e.target.remove();
+    storeList();
+});
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    // on appel la fonction addMessage a chaque soumission du formulaire  avec le paramettre pbridge qui de vide est passé a e.target.value
+    // on appel la fonction addMessage à chaque soumission du formulaire  avec le paramettre pbridge qui de vide est passé a e.target.value
     if (input.value.length < 2) {
         alert('votre message doit faire au moins 2 caractères');
         return false;
     }
     else {
-        addMessage(pbridge);
+        messageBox.innerHTML += `<p>${input.value}</p>`;
+        localStorage.setItem('value', input.value);
         // on remet la valeur interne de l'input a 0
-        // input = null
-        // on enleve ce qu'on a tapé précédemment dans l'input 
         input.value = "";
+        // on actualise la liste dans le localstorage 
+        storeList();
     }
 });
--- index.ts
+++ index.ts
@@ -1,42 +1,42 @@
 const form:any = document.querySelector('form');
 let input:any = document.querySelector("input[type='text']")
 const messageBox:any = document.querySelector(".message-container")
-// variable global qui va servir de pont entre notre e.target.value de notre input et notre fonction   
-let pbridge = "";
-let pArray:any = []
 
-input.addEventListener('input', (e:any,valueMessage:any)=>{
-    pbridge = e.target.value;
+// sauve garder dans le locale storage 
+function storeList(){
+    // todolist est le nom qu'on a donné a notre key ds le localstorage 
+    window.localStorage.todoList = messageBox.innerHTML;
+}
+// récupérer les donner dans le local storage 
+function getStorage(){
+   
+   if( window.localStorage.todoList){
+    messageBox.innerHTML =  window.localStorage.todoList;
+   }
+   else{
+    messageBox.innerHTML = `<p>cliquez sur un todo pour le supprimer</p>`
+   }
+}
 
-})
+// au chargement 
+window.addEventListener('load', getStorage)
+
+
 
 // je crée une fonction qui va prendre en paramettre notre futur e.target.value car en js les fonctions sont jouées en 1er  
-const addMessage = (valueMessage:any) =>{
-   let p:any = document.createElement('p');
-    p.innerHTML=valueMessage;
-    messageBox.appendChild(p);
-    localStorage.setItem('value', valueMessage)
+// const addMessage = (valueMessage:any) =>{
+   
+    
+//  }
 
-
-    p.addEventListener('click', (e:any)=>{
-         e.target.remove()
-         localStorage.removeItem('value')
-    })
- }
-
-
- 
-
- 
-
- 
-         
-     
- 
+ messageBox.addEventListener('click', (e:any)=>{
+        e.target.remove()
+        storeList()
+ })
 
 form.addEventListener('submit', (e:any)=>{
     e.preventDefault()
-    // on appel la fonction addMessage a chaque soumission du formulaire  avec le paramettre pbridge qui de vide est passé a e.target.value
+    // on appel la fonction addMessage à chaque soumission du formulaire  avec le paramettre pbridge qui de vide est passé a e.target.value
 
     if(input.value.length < 2) {
         alert('votre message doit faire au moins 2 caractères')
@@ -44,11 +44,12 @@ form.addEventListener('submit', (e:any)=>{
 
     } else{
 
-        addMessage(pbridge)
+        messageBox.innerHTML += `<p>${input.value}</p>`;
+    localStorage.setItem('value',input.value )
         // on remet la valeur interne de l'input a 0
-        // input = null
-        // on enleve ce qu'on a tapé précédemment dans l'input 
         input.value="";
+        // on actualise la liste dans le localstorage 
+        storeList()
     }
 
 })
